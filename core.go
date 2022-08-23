a6827ba3066e388abab61d97b331601b0b90ab41
mmmmx <32198721+c3b2a7@users.noreply.github.com>
2022-08-23 16:22:44 +0800

    Add remote and dynamic port forwarding (#2)
    * add readme
    
    * Create LICENSE (#1)
    
    * Add remote and dynamic port forwarding
    
    * Update README.md

--- core.go
+++ core.go
@@ -3,18 +3,15 @@ package main
 import (
 	"errors"
 	"fmt"
+	"github.com/c3b2a7/sshtunnel/socks"
 	"golang.org/x/crypto/ssh"
 	"io"
 	"net"
 	"os"
+	"strings"
 	"sync"
 	"time"
 )
-
-var app struct {
-	Verbose bool
-	Config  Config
-}
 
 type Endpoint struct {
 	Host, Port string
@@ -24,13 +21,48 @@ func (e *Endpoint) String() string {
 	return fmt.Sprintf("%s:%s", e.Host, e.Port)
 }
 
+type Mode int
+
+func (m Mode) String() string {
+	switch m {
+	case Local:
+		return "LOCAL"
+	case Remote:
+		return "REMOTE"
+	case Dynamic:
+		return "DYNAMIC"
+	}
+	return "UNSUPPORTED"
+}
+
+const (
+	Local Mode = 1 << iota
+	Remote
+	Dynamic
+)
+
+var ModeNotSupported = errors.New("mode not supported, available: local, remote, dynamic")
+
+func PickMode(mode string) (Mode, error) {
+	mode = strings.ToUpper(mode)
+	switch mode {
+	case "LOCAL":
+		return Local, nil
+	case "REMOTE":
+		return Remote, nil
+	case "DYNAMIC":
+		return Dynamic, nil
+	}
+	return -1, ModeNotSupported
+}
+
 type Tunnel struct {
 	Remote Endpoint
 	Local  Endpoint
+	Mode   Mode
 }
 
 type Config struct {
-	Tunnels    []Tunnel
 	Target     Endpoint
 	Username   string
 	PrivateKey []byte
@@ -68,7 +100,7 @@ func Connect(config Config) (*ssh.Client, error) {
 
 // Bridge
 func (t Tunnel) Bridge(conn *ssh.Client) error {
-	l, err := net.Listen("tcp", t.Local.String())
+	l, err := listen(t, conn)
 	if err != nil {
 		return err
 	}
@@ -82,19 +114,57 @@ func (t Tunnel) Bridge(conn *ssh.Client) error {
 
 		go func() {
 			defer c.Close()
-			rc, err := conn.Dial("tcp", t.Remote.String())
+			var rc net.Conn
+			var local, remote string
+
+			switch t.Mode {
+			case Local:
+				local, remote = t.Local.String(), t.Remote.String()
+				rc, err = conn.Dial("tcp", remote)
+			case Remote:
+				local, remote = t.Remote.String(), t.Local.String()
+				rc, err = net.Dial("tcp", remote)
+			case Dynamic:
+				tgt, err := socks.Handshake(c)
+				if err != nil && err == socks.InfoUDPAssociate {
+					buf := make([]byte, 1)
+					for {
+						// block here
+						_, err := c.Read(buf)
+						if err, ok := err.(net.Error); ok && err.Timeout() {
+							continue
+						}
+						logger.Printf("UDP Associate End.")
+						return
+					}
+				}
+				local, remote = t.Local.String(), tgt.String()
+				rc, err = conn.Dial("tcp", remote)
+			default:
+				err = ModeNotSupported
+			}
+
 			if err != nil {
-				logger.Printf("failed to connect to remote %s via server %s: %v", t.Remote, conn.RemoteAddr(), err)
+				logger.Printf("failed to connect to remote %s via server %s: %v", remote, conn.RemoteAddr(), err)
 				return
 			}
 			defer rc.Close()
-
-			logger.Printf("tunneling %s <-> %s <-> %s", c.LocalAddr(), conn.RemoteAddr(), t.Remote.String())
+			logger.Printf("tunneling(%s) %s <-> %s <-> %s", t.Mode.String(), local, conn.RemoteAddr(), remote)
 			if err = relay(rc, c); err != nil {
 				logger.Printf("relay error: %v", err)
 			}
 		}()
 	}
+}
+
+func listen(t Tunnel, conn *ssh.Client) (net.Listener, error) {
+	switch t.Mode {
+	case Local, Dynamic:
+		return net.Listen("tcp", t.Local.String())
+	case Remote:
+		return conn.Listen("tcp", t.Remote.String())
+	}
+	return nil, ModeNotSupported
 }
 
 // relay copies between left and right bidirectionally
